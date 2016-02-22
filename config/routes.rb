ad2184234f020a5feb73c85d3340dff00d6f498c
Ludovic Boisnon <lboisnon@gmail.com>
2016-02-22 23:19:38 +0100

    fixed bug in account update missing div

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,12 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root to: "links#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root to: "links#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
